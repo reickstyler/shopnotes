2b6727c456fa3513c066db65f6fd5043558294b7
Tyler Reicks <tyler.reicks@gmail.com>
2020-12-14 23:40:37 -0700

    Initial commit for list project.

---
+++ functions/create-shopnote.js
@@ -0,0 +1,36 @@
+const query = require("./utils/query");
+
+const CREATE_SHOPNOTE = `
+  mutation($name: String!, $description: String!, $updatedAt: Time!, $items: ShopNoteItemsRelation!) {
+    createShopNote(data: {name: $name, description: $description, updatedAt: $updatedAt, items: $items}) {
+      _id
+      name
+      description
+      updatedAt
+      items {
+        data {
+          name,
+          checked,
+          urgent
+        }
+      }
+    }
+  }
+`;
+
+exports.handler = async (event) => {
+  const { name, items } = JSON.parse(event.body);
+  const { data, errors } = await query(CREATE_SHOPNOTE, { name, items });
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ shopnote: data.createShopNote }),
+  };
+};
